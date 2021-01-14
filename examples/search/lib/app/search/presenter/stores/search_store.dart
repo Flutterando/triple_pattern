83a888fc70075ff5036be466fa776b6f0a5c4ffb
Jacob Moura <jacobaraujo7@gmail.com>
2021-01-14 18:48:02 -0300

    .

--- examples/search/lib/app/search/presenter/stores/search_store.dart
+++ examples/search/lib/app/search/presenter/stores/search_store.dart
@@ -15,4 +15,13 @@ class SearchStore extends MobXStore<Failure, List<Result>> {
   void setSearchText(String value) {
     executeEither(() => searchByText(value), delay: Duration(milliseconds: 500));
   }
+
+  @override
+  Triple<Failure, List<Result>> middleware(Triple<Failure, List<Result>> newTriple) {
+    if (newTriple.event == TripleEvent.state) {
+      if (newTriple.state.isEmpty) return newTriple.copyWith(event: TripleEvent.error, error: EmptyList());
+    }
+
+    return newTriple;
+  }
 }
