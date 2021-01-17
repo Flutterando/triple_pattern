a1721af09999b5f1c17767808f72e3d5dc517881
Jacob Moura <jacobaraujo7@gmail.com>
2021-01-17 16:30:48 -0300

    added filter

--- stores/flutter_triple/lib/src/scoped_builder.dart
+++ stores/flutter_triple/lib/src/scoped_builder.dart
@@ -4,12 +4,13 @@ import 'package:triple/triple.dart';
 
 class ScopedBuilder<TStore extends Store<TError, TState>, TError extends Object, TState extends Object> extends StatefulWidget {
   final dynamic Function(TState state)? distinct;
+  final bool Function(TState state)? filter;
   final Widget Function(BuildContext context, TState state)? onState;
   final Widget Function(BuildContext context, TError? error)? onError;
   final Widget Function(BuildContext context)? onLoading;
   final TStore store;
 
-  const ScopedBuilder({Key? key, this.distinct, this.onState, this.onError, this.onLoading, required this.store})
+  const ScopedBuilder({Key? key, this.distinct, this.filter, this.onState, this.onError, this.onLoading, required this.store})
       : assert(onState != null || onError != null || onLoading != null, 'Define at least one listener (onState, onError or onLoading)'),
         super(key: key);
 
@@ -36,15 +37,14 @@ class _ScopedBuilderState<TStore extends Store<TError, TState>, TError extends O
     disposer = widget.store.observer(
       onState: (state) {
         final value = widget.distinct?.call(state);
-
-        bool isReload;
-        if (value is List) {
-          isReload = !eq(value, _distinct);
-        } else {
-          isReload = value != _distinct;
+        bool isReload = true;
+        if (value != null) {
+          isReload = value is List ? !eq(value, _distinct) : value != _distinct;
         }
         _distinct = value;
-        if (widget.onState != null && !isDisposed && isReload) {
+
+        final filter = widget.filter?.call(state) ?? true;
+        if (widget.onState != null && !isDisposed && isReload && filter) {
           setState(() {
             child = widget.onState?.call(context, state);
           });
